5d247b86140a51f5521c129b64c5a2a700617265
deboch <dschamorro_90@hotmail.com>
2020-05-18 19:39:51 -0300

    Layouts,partial view y Template
    BaseLayout, AnonimoLayout, NecesidadesLayout, pv_Footer y template de prueba

--- Entidades/Usuario.cs
+++ Entidades/Usuario.cs
@@ -12,12 +12,12 @@ namespace Entidades
             public int Id { get; set; }
             public string Nombre { get; set; }
             public string Apellido { get; set; }
-            public DateTime fechaNacimiento { get; set; }
-            public string Username { get; set; }
+            public DateTime FechaNacimiento { get; set; }
+            public string UserName { get; set; }
             public string Email { get; set; }
             public string Password { get; set; }
             public string TipoUsuario { get; set; }
-            public DateTime fechaCreacion { get; set; }
+            public DateTime FechaCreacion { get; set; }
             public bool Activo { get; set; }
             public string Token { get; set; }
     
--- ayudandoALaPandemia/Controllers/HomeController.cs
+++ ayudandoALaPandemia/Controllers/HomeController.cs
@@ -8,9 +8,24 @@ namespace ayudandoALaPandemia.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
+        //public string Index()
+        //{
+        // return "hola";
+        //}
+
+        [HttpGet]
+        public ActionResult Index()
         {
-            return "hola";
+            Entidades.Necesidad necesidad1 = new Entidades.Necesidad(1, "uno", "");
+            Entidades.Necesidad necesidad2 = new Entidades.Necesidad(1, "uno", "");
+            Entidades.Necesidad necesidad3 = new Entidades.Necesidad(1, "uno", "");
+
+            List<Entidades.Necesidad> necesidades = new List<Entidades.Necesidad>();
+            necesidades.Add(necesidad1);
+            necesidades.Add(necesidad2);
+            necesidades.Add(necesidad3);
+          
+            return View(necesidades);
         }
         
     }
