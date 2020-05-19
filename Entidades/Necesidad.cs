534e12b25fd0b8e933a5f182633c634ab89944da
deboch <dschamorro_90@hotmail.com>
2020-05-19 13:30:12 -0300

    Lista ordenada en home

--- Entidades/Necesidad.cs
+++ Entidades/Necesidad.cs
@@ -11,11 +11,15 @@ namespace Entidades
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public bool Estado { get; set; }
+        public int Valoracion { get; set; }
 
-        public Necesidad (int id, string name, string description) {
+        public Necesidad (int id, string name, string description, bool estado, int valoracion) {
             this.Id = id;
             this.Name = name;
             this.Description = description;
+            this.Estado = estado;
+            this.Valoracion = valoracion;
         }
     }
 }
--- ayudandoALaPandemia/Controllers/HomeController.cs
+++ ayudandoALaPandemia/Controllers/HomeController.cs
@@ -16,16 +16,25 @@ namespace ayudandoALaPandemia.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-            Entidades.Necesidad necesidad1 = new Entidades.Necesidad(1, "uno", "");
-            Entidades.Necesidad necesidad2 = new Entidades.Necesidad(1, "uno", "");
-            Entidades.Necesidad necesidad3 = new Entidades.Necesidad(1, "uno", "");
+            Entidades.Necesidad necesidad1 = new Entidades.Necesidad(1, "uno", "", true, 5);
+            Entidades.Necesidad necesidad2 = new Entidades.Necesidad(2, "ds", "", true, 4);
+            Entidades.Necesidad necesidad3 = new Entidades.Necesidad(3, "fdfh", "", true, 2);
+            Entidades.Necesidad necesidad4 = new Entidades.Necesidad(4, "fghfg", "", true, 5);
+            Entidades.Necesidad necesidad5 = new Entidades.Necesidad(5, "ufhghno", "", true, 4);
+            Entidades.Necesidad necesidad6 = new Entidades.Necesidad(6, "fgfj", "", true, 2);
 
             List<Entidades.Necesidad> necesidades = new List<Entidades.Necesidad>();
+            List<Entidades.Necesidad> masValoradas = new List<Entidades.Necesidad>();
             necesidades.Add(necesidad1);
             necesidades.Add(necesidad2);
             necesidades.Add(necesidad3);
-          
-            return View(necesidades);
+            necesidades.Add(necesidad4);
+            necesidades.Add(necesidad5);
+            necesidades.Add(necesidad6);
+
+            var listaOrdenada = necesidades.OrderByDescending(x => x.Valoracion).ToList();
+            
+            return View(listaOrdenada);
         }
         
     }
