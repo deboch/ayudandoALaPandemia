a6225fdc86bc859bf4ee9967db6bed0f9b8f99ec
PABLO SANCHEZ <pabl1sanchez@alumno.unlam.edu.ar>
2020-06-11 19:58:27 -0300

    agrego apinecesidadescontroller

---
+++ ayudandoALaPandemia/Controllers/ApiNecesidadesController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using Repositorios;
+
+
+namespace ayudandoALaPandemia.Controllers
+{
+    public class ApiNecesidadesController : BaseController
+    {
+        public string necesidades(string k)
+        {
+            int userId = ((int)Session["id"]);
+            string keyword = Request.QueryString["keyword"];
+            List<Necesidades> necesidades = searchServicios.ObtenerNecesidades(userId, keyword);
+            return JsonConvert.SerializeObject(necesidades, Formatting.Indented,
+                    new JsonSerializerSettings
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                    });
+        }
+
+    }
+}
