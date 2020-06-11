193174924fa45426e7976bb0d8b85d365188378c
deboch <dschamorro_90@hotmail.com>
2020-06-11 15:48:32 -0300

    Se agrega Template

--- ayudandoALaPandemia/App_Start/BundleConfig.cs
+++ ayudandoALaPandemia/App_Start/BundleConfig.cs
@@ -13,6 +13,12 @@ namespace ayudandoALaPandemia
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Template/js/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Template/js/jquery.validate*"));
+
             // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
             // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,6 +30,15 @@ namespace ayudandoALaPandemia
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Template/js/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Template/css").Include(
+                      "~/Template/bootstrap.css",
+                      "~/Template/site.css"));
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
