9d7b6cc86f3dbb9b3c8cb1873e9b2c13b86f7996
deboch <dschamorro_90@hotmail.com>
2020-06-22 23:21:21 -0300

    Auto stash before merge of "feacture/front" and "origin/feacture/front"

---
+++ ayudandoALaPandemia/Builder/DenunciaBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ayudandoALaPandemia.ViewModels;
+using Repositorios;
+
+
+namespace ayudandoALaPandemia.Builder
+{
+    public class DenunciaBuilder
+    {
+        public Denuncias toDenunciasEntity(DenunciaDto denunciaDto, int userId)
+        {
+            Denuncias nuevaDenuncia = new Denuncias();
+            //nuevaDenuncia.IdNecesidad = necesidadId;
+            MotivoDenuncia motivo = new MotivoDenuncia();
+            motivo.IdMotivoDenuncia = denunciaDto.MotivoDenuncia;
+            nuevaDenuncia.MotivoDenuncias.Add(motivo);
+            nuevaDenuncia.Comentarios = denunciaDto.Comentarios;
+            nuevaDenuncia.IdUsuario = userId;
+            nuevaDenuncia.FechaCreacion = Convert.ToDateTime(DateTime.Now.ToString("dd-MMM-yyyy"));
+            nuevaDenuncia.Estado = denunciaDto.Estado;
+            return nuevaDenuncia;
+        }
+    }
+}
