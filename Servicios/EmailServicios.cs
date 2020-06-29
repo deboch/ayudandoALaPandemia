68180d16b2af3028947beeba9b72a3c79670b3df
Gonzalo <gagustin.sk@gmail.com>
2020-06-29 20:28:04 -0300

    cositas que me olvidé commitear, japish japish

---
+++ Servicios/EmailServicios.cs
@@ -0,0 +1,38 @@
+﻿using Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class EmailServicios
+    {
+        ManagerRepository managerRepository = new ManagerRepository();
+
+        public void sendEmail(string token, string email)
+        {
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("ayudandoalapandemia.19@gmail.com", "covid2020"),
+                EnableSsl = true,
+            };
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress("ayudandoalapandemia.19@gmail.com"),
+                Subject = "Validación de cuenta",
+                Body = $"<a href='https://localhost:44306/Registro/activoUsuario?token={token}'>https://localhost:44306/Registro/activoUsuario?token={token}</a>",
+                IsBodyHtml = true,
+            };
+             mailMessage.To.Add("gagustin.sk@gmail.com"); // Para testear 
+             //mailMessage.To.Add(email); // Productivo
+
+            smtpClient.Send(mailMessage);
+
+        }
+    }
+}
